85395f13a3b343c3a2723bd1f1c7dc8d4ff67dbe
luke gorman <lukegorman@gmail.com>
2022-01-14 10:14:45 +0000

    tidy up, add axis labels

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { MatTableDataSource, MatTableModule } from '@angular/material/table';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { DataSource } from '@angular/cdk/collections';
-
+import { MatTableDataSource } from '@angular/material/table';
+import { HttpClient } from '@angular/common/http';
 
 
 @Component({
@@ -24,37 +22,25 @@ export class AppComponent implements OnInit {
 
 
   title = 'noc';
-  displayedColumns: string[] = ['name', 'lat', 'lng' /*, 'name'*/];
+  displayedColumns: string[] = ['name', 'lat', 'lng'];
   _url = "https://apps.noc-innovations.co.uk/api/polpred-api/latest/get-port-list";
 
   layout = {
-
     autosize: false,
-
     width: 500,
-
     height: 500,
-
     yaxis: {
-
-      title: 'Y-axis Title',
-
-      ticktext: ['long label', 'Very long label', '3', 'label'],
-
-      tickvals: [1, 2, 3, 4],
-
-      tickmode: 'array',
-
+      title: 'longitude',
       automargin: true,
-
       titlefont: { size: 30 },
-
     },
-
+    xaxis: {
+      title: 'latitude',
+      automargin: true,
+      titlefont: { size: 30 },
+    },
     paper_bgcolor: '#7f7f7f',
-
     plot_bgcolor: '#c7c7c7'
-
   };
 
   public graph = {
@@ -65,63 +51,12 @@ export class AppComponent implements OnInit {
     layout: this.layout
   };
 
-  //@ViewChild(MatSort) sort: MatSort;
-  /*dataSource = this.http.get<any>(this._url).subscribe(response => {
-    this.data = response.data;;
-  });*/
-
-
-  /*
-  {
-  "data": {
-    "items": [
-      {
-        "name": "St. Helier",
-        "lat": 49.18333,
-        "lng": -2.11667
-      },
-      {
-        "name": "St. Mary's",
-        "lat": 49.91833,
-        "lng": -6.31667
-      },
-      {
-        "name": "Newlyn",
-        "lat": 50.10333,
-        "lng": -5.54333
-      },
-      {
-        "name": "Plymouth",
-        "lat": 50.36833,
-        "lng": -4.185
-      },
-      {
-        "name": "Weymouth",
-        "lat": 50.60833,
-        "lng": -2.44833
-      },
-      { "etc..." },
-    ]
-  }
-}
-  */
-
-
   ngOnInit() {
     this.http.get<any>(this._url).subscribe(response => {
-
-      //console.log(array)
       this.rawdata = response.data.items;
-
       this.rawdata.forEach(element => this.lat.push(element.lat));
       this.rawdata.forEach(element => this.long.push(element.lng));
-
-      this.dataSource = new MatTableDataSource(response.data.items/*, ...response.data.licences.details.model*/);
-      console.log(this.lat, this.long);
-  
-
-
-      //this.dataSource.sort = this.sort;
+      this.dataSource = new MatTableDataSource(response.data.items);
     });
   }
 
